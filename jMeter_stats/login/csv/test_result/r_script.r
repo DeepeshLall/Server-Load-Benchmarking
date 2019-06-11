83e0e3379675c64e3d01c7cc4df0233a90ebfa8f
srinjayk <srinjayk@iitk.ac.in>
2019-06-10 20:17:04 -0400

    update

--- jMeter_stats/login/csv/test_result/r_script.r
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/Rscript
-args=commandArgs(trailingOnly=TRUE)
-library(ggplot2)
-t=read.csv(args[1])
-print(t)
-p<-ggplot(t)+geom_line(aes(x=user_number,y=avg_latency_time,color=url_number))+labs(x='user number',y='Average latency time',title='latency vs per user for differrent pages')+facet_grid(url_number~.)
-ggsave(p,file = paste0(args[3], args[2], ".png"),device = "png")
