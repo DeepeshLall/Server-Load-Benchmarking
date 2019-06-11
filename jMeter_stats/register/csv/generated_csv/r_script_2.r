55d502c820e2b6d7de84562f4c31303de302f242
srinjayk <srinjayk@iitk.ac.in>
2019-06-10 22:10:27 -0400

    Update

--- jMeter_stats/register/csv/generated_csv/r_script_2.r
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/Rscript
-args=commandArgs(trailingOnly=TRUE)
-library(ggplot2)
-t=read.csv(args[1])
-print(t)
-p<-ggplot(t)+geom_line(aes(x=no_of_user,y=latency_time,color=url_no))+labs(x='number of users',y='latency time',title='latency vs number of users')+facet_grid(url_no~.)
-ggsave(p,file = paste0(args[3], args[2], ".png"),device = "png")
